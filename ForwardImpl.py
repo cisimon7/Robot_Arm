b0b585654e44551254d6694d12d3fbfcbbfe7800
cisimon7 <simontaneous1@hotmail.com>
2020-10-13 12:25:34 +0100

    Commit 2

--- ForwardImpl.py
+++ ForwardImpl.py
@@ -3,15 +3,17 @@ from sympy import trigsimp, lambdify
 from utils import *
 
 # Joint parameters declaration
-q1, q2, a2, d1, d3 = symbols("q1 q2 a2, d1 d3", real=True)
+q1, q2, q3, q4, q5 = symbols("q1 q2 q3 q4 q5", real=True)
 
-# Forward Kinematics Homogeneous Matrix
-#   Rz(q1)*Tz(d1)*Rx(q2)*Ty(a2)*Ty(d3)
-H = Rz(q1)*Tz(d1)*Rx(q2)*Ty(a2)*Ty(d3)
+# Guesses for link lengths
+l1, l2, l3, l4, l5 = 1, 1, 1, 1, 1
+
+# Forward Kinematics
+H = Rx(q1)*Tx(l1)*Ry(q2)*Tz(l2)*Rz(q3)*Tz(l3)*Rx(q4)*Tz(l4)*Rz(q5)*Tz(l5)
 H = trigsimp(H)
 
 set_printoptions(suppress=True)
 
 
 def forwardKinematics():
-    return lambdify([(q1, q2, a2, d1, d3)], H, "numpy")
+    return lambdify([(q1, q2, q3, q4, q5)], H, "numpy")
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from ForwardImpl import forwardKinematics
+from math import pi
+from numpy import round
 
 
-# Press the green button in the gutter to run the script.
+def forward_tests():
+    # test cases
+    # you can add more test cases here
+    # angles input should be in degrees
+    test_cases = [
+        # q1, q2, q3, q4, q5
+        [0, 0, 0, 0, 0],
+        [30, 50, 20, 10, 20]
+    ]
+
+    f_function = forwardKinematics()
+
+    for i, param in enumerate(test_cases):
+        h = f_function((
+            (param[0] * (pi/180)),  # q1
+            (param[1] * (pi/180)),  # q2
+            param[2],  # a2
+            param[3],  # d1
+            param[4]   # d3
+        ))
+
+        view = f"CASE {i + 1}:\n{round(h, 4)}\n"
+        print(view)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    forward_tests()
